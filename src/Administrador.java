1ec562a73d1a0caf45b71a08283a064959355332
Andres <pereithasonbata@gmail.com>
2023-10-05 11:59:05 -0500

    creando clase de administrador

--- src/Administrador.java
+++ src/Administrador.java
@@ -1,4 +1,30 @@
 public class Administrador {
 
+String nombre;
+int edad;
+String rol;
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
 }
