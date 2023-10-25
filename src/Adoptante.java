c57c62422f547deea4704f712bf23956cbfd7a92
Machadoalejo <machadoalejo@gmail.com>
2023-10-25 13:25:53 -0500

    Creando atributos y metodo constructor

---
+++ src/Adoptante.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Adoptante {
+    private int id;
+    private String name;
+    private String address;
+    private String contactNumber;
+    private String adoptionPreferences;
+
+    public Adoptante() {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.contactNumber = contactNumber;
+        this.adoptionPreferences = adoptionPreferences;
+    }
+}
