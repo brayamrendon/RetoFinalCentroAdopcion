47f3a4b9eefeb6b6fafebe5d771be51c25e6cc88
Wdjway <djway20@gmail.com>
2023-10-25 11:35:20 -0500

    Creando atributos, metodo constructor y los metodos set y get

--- src/Empleados.java
+++ src/Empleados.java
@@ -1,3 +1,47 @@
-public class Empleados {
+import java.time.LocalDate;
 
+public class Empleados {
+    private int id;
+    private String name;
+    private String rol;
+    private LocalDate dateOfHire;
+
+    public Empleado() {
+        this.id = id;
+        this.name = name;
+        this.rol = rol;
+        this.dateOfHire = dateOfHire;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public LocalDate getDateOfHire() {
+        return dateOfHire;
+    }
+
+    public void setDateOfHire(LocalDate dateOfHire) {
+        this.dateOfHire = dateOfHire;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
