7c14d8a4bd600b19534315902b39aa0a5a9a160c
Wdjway <djway20@gmail.com>
2023-10-25 11:43:00 -0500

    creando metodo create para añadir un nuevo empleado

--- src/Empleados.java
+++ src/Empleados.java
@@ -1,4 +1,8 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+
 
 public class Empleados {
     private int id;
@@ -43,5 +47,33 @@ public class Empleados {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    // Lista para almacenar empleados
+    private static List<Empleado> empleadoList = new ArrayList<>();
+
+    public static void createEmpleado(Scanner scanner) {
+        Empleado empleado = new Empleado();
+        System.out.println("Creación de Empleado:");
+
+        // Solicitar información del empleado al usuario
+        System.out.print("Nombre del empleado: ");
+        empleado.setName  (scanner.nextLine());
+        System.out.print("Rol del empleado: ");
+        empleado.setRol (scanner.nextLine());
+        System.out.print("Fecha de contratación (YYYY-MM-DD): ");
+        String dateOfHireString = scanner.nextLine();
+
+        // Convertir la cadena de fecha a LocalDate
+        LocalDate.parse(dateOfHireString);
+
+        // Genera un ID único en secuencia (puedes implementar tu propia lógica)
+        empleado.setId(empleadoList.size() + 1);
+
+        empleadoList.add(empleado);
+        System.out.println("Empleado registrado con éxito.");
+
+        // Llama al método para crear una nueva hoja en el archivo Excel
+        Tools.createSheetForEmpleado(empleado);
     }
 }
